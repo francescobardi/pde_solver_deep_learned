0e35b4d26614bc631a2c3e355c59ea779a2e5ed0
Samuel von Baussnern <samuel.vonbaussnern@gmail.com>
2018-11-29 22:42:39 +0100

    model that runs, not a running model

--- nnpde/main.py
+++ nnpde/main.py
@@ -46,24 +46,21 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# <codecell>
+from functools import reduce
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        
-        # should be `nn.Conv2d(???, ???, 3, bias=False)` in our case
-        self.conv1 = nn.Conv2d(1, 1, 3, bias=False) 
-        
-    def forward(self, x):
-        x = self.conv1(x)
-        
-        return x
+from scipy.linalg import circulant
 
-# <codecell>
+import numpy as np
 
 from nnpde.functions import iterativeMethods as im
-import numpy as np
+
+# <codecell>
+
+A.shape
+
+# <codecell>
+
+u.shape
 
 # <codecell>
 
@@ -90,7 +87,43 @@ u, res = im.jacobi(A, f, b_idx = b_idx, b = b, max_iters=200,tol = 1e-2)
 
 # <codecell>
 
-u
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        # should be `nn.Conv2d(???, ???, 3, bias=False)` in our case
+        self.conv1 = nn.Conv2d(1, 1, 3, bias=False) 
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        
+        return x
+
+net = Net()
+H = net(torch.ones(1, 1, 5, 5))
+H
+
+# <codecell>
+
+def reshape_H(H, N=N):
+    # Given our definition of the net we get a 3x3 matrix, or 9x1 vector.
+    # N is 10 (might of course change), therefor the shape of H should be N**2 x N**2
+    # As H is a circulant matrix (by design) we transform it to that shape:
+    #  - add 1 as the first element -> results in a Nx1 vector
+    #  - add 0 as the padding -> results in a N**2x1 vector
+    #  - rotate H (rot1) N times -> results in a N**2 N**2x1 vectors
+    #  - stack thoes new columns -> results in a N**2xN**2 matrix
+    dim = N**2
+    z = torch.zeros(dim) # pytorch can save the gradients... do not use the flag here, it will result in a "leaf moved..." error
+    z[0] = torch.ones(1)
+    z[1:N] = H.view(-1)
+    
+    idx_of_circulant = circulant(range(dim))
+
+    # no fucking clue if this is right... it will result in the correct shape
+    return torch.cat([z[idx_of_circulant[:, i]].view((dim, 1)) for i in range(dim)], dim=1)
+
+reshape_H(H)
 
 # <codecell>
 
@@ -103,11 +136,13 @@ def reset_boundaries(X, G=G):
 
 
 def iterator_step(H, T=T, G=G):
+    """will define the matrix computation"""
     return reset_boundaries(T + H@T - H, G=G)
     
     
-def iterations(u0, k, H, T=T, G=G):
+def solver_with_H(u0, k, H, T=T, G=G):
     X = iterator_step(H, T=T, G=G)
+    # this is basically a fold-left, the iterator step will be applied k times: X(X(X(... (X(u0)))))
     return F.reduce(lambda acc, el: el(acc), [(lambda u: X.mm(u)) for _ in range(k)], u0)
 
 
@@ -118,20 +153,63 @@ class CustLoss(nn.Module):
         self.k = k
         # should be u0 and u*
         # TODO `0` is the initial value, is this correct?
-        self.u_stars = [(0, u_star) for u_star in u_stars]
+        #self.u_stars = [(0, u_star) for u_star in u_stars]
+        self.u_stars = [(torch.zeros(N**2, 1), u_star) for u_star in u_stars]
         
     def forward(self, yPred, yTrue):
         # we don't care about y, as this would be y_true??? or is it x???
         
-        H = torch.from_numpy(yPred).requires_grad(True)
+        #H = torch.from_numpy(yPred).requires_grad(True)
     
         # since we doing for all u_stars we can do it probably in a better way (vectorised)
-        return torch.sum[(torch.abs(iterations(u0, k=self.k, H=yPred) - u_star) for u0, u_star in self.u_stars)]
+        # TODO think about the aggregation of the loss
+        return torch.sum(torch.cat([torch.pow(solver_with_H(u0, k=self.k, H=reshape_H(yPred)) - u_star, 2) for u0, u_star in self.u_stars]))
+        #return torch.sum(torch.cat([torch.abs(solver_with_H(u0, k=self.k, H=reshape_H(yPred)) - u_star) for u0, u_star in self.u_stars]))
+
+# <codecell>
+
+from torch.autograd import Variable
+import torch.optim as optim
+
+loss_fn = CustLoss(Variable(torch.from_numpy(u.reshape(N, N)).float()))
+net = Net()
+H = net(torch.ones(1, 1, 5, 5)) # just ones as some initial values (can be anything but 0, can it? 0 for sure not...)
+ground_truth = torch.ones(1, 1, 5, 5) # since the output of the model will be H, which we don't know, so we don't care about the output
+
+loss = loss_fn(H, ground_truth)
+
+net.zero_grad() # H is the model
+print("pre backward prop", net.conv1.weight)
+loss.backward()
+print("post backward prop", net.conv1.weight)
+#optimizer.step()
+
+# <codecell>
+
+optimizer = optim.Adam(net.parameters())
+
+
+# well... as we can see nothing happens...
+H = net(torch.ones(1, 1, 5, 5)) # just ones as some initial values (can be anything but 0, can it? 0 for sure not...)
+ground_truth = torch.ones(1, 1, 5, 5) # since the output of the model will be H, which we don't know, so we don't care about the output
+optimizer.zero_grad()
+loss = loss_fn(H, ground_truth)
+loss.backward()
+optimizer.step()
+
+print("post backward prop", net.conv1.weight)
+
+# <codecell>
+
+net(torch.ones(1, 1, 5, 5))
+
+# <codecell>
+
+loss
 
 # <codecell>
 
 loss_fn = CustLoss(u)
-
 
 prediction = jacobi(A, b, H)
 loss = loss_fn(prediction, ground_truth)
