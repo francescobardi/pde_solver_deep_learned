e08f06b5357dd1edf60b254c471dfd0e4af863b0
Samuel von Baussnern <samuel.vonbaussnern@gmail.com>
2018-12-13 16:23:00 +0100

    fixes bug with lshape, adds different values for each boundary side

--- nnpde/nnpde/functions/geometries.py
+++ nnpde/nnpde/functions/geometries.py
@@ -2,9 +2,15 @@ import numpy as np
 import torch
 
 
-def square_geometry(size, boundary_value=None):
+def square_geometry(size):
     """Defines a square geometry of given size (as a single positive integer)
-    If boundary_value is None a random value will between [1, 4) will be chosen
+    The boundary-values are set randomly.
+
+    Example:
+
+    b1 b1 b2
+    b4 0  b2
+    b4 b3 b3
 
     Returns:
         (geometry, boundary_index)
@@ -12,21 +18,24 @@ def square_geometry(size, boundary_value=None):
 
     # Define geometry 1.0 inner points 0.0 elsewhre
     B_idx = torch.ones(1, 1, size ,size )
-    B_idx[0,0,0,:] = torch.zeros(size)
-    B_idx[0,0,size-1,:] = torch.zeros(size)
-    B_idx[0,0,:,0] = torch.zeros(size)
-    B_idx[0,0,:,size -1] = torch.zeros(size)
+    B_idx[0,0,0 ,:] = torch.zeros(size)
+    B_idx[0,0,-1,:] = torch.zeros(size)
+    B_idx[0,0,:, 0] = torch.zeros(size)
+    B_idx[0,0,:,-1] = torch.zeros(size)
 
     # Define boundary values
-    B = torch.abs(B_idx-1)*(boundary_value or (np.random.normal(0,1)))
+    B = torch.zeros_like(B_idx)
 
+    B[0,0,0 ,:] = np.random.normal(0, 1)
+    B[0,0,:,-1] = np.random.normal(0, 1)
+    B[0,0,-1,:] = np.random.normal(0, 1)
+    B[0,0,1:, 0] = np.random.normal(0, 1) # we don't want to overwrite the first value
     return B_idx, B
 
 
-def l_shaped_geometry(size, l_cutout_size=None, boundary_value=None):
+def l_shaped_geometry(size, l_cutout_size=None):
     """Defines a L-shaped geometry of given size (as a single positive integer), and
     l_cutout_size (think of creating the L-shape as cutting out a smaller square piece)
-    If boundary_value is None a random value will between [1, 4) will be chosen.
 
     l_cutout_size is by default size/2
 
@@ -36,15 +45,12 @@ def l_shaped_geometry(size, l_cutout_size=None, boundary_value=None):
 
     l_cutout_size = l_cutout_size or int(np.floor(size/2))
 
-    # Define geometry 1.0 inner points 0.0 elsewhre
-    B_idx = torch.ones(1, 1, size, size)
-    B_idx[0,0,0:l_cutout_size,0:l_cutout_size] = \
-            torch.zeros([l_cutout_size, l_cutout_size])
-    B_idx[0,0,size-1,:] = torch.zeros(size)
-    B_idx[0,0,:,0] = torch.zeros(size)
-    B_idx[0,0,:,size-1] = torch.zeros(size)
+    B_idx, B = square_geometry(size)
 
-    # Define boundary values
-    B = torch.abs(B_idx-1)*(boundary_value or (np.random.normal(0,1)))
+    _, cutout = square_geometry(l_cutout_size)
+    B[0, 0, :l_cutout_size, :l_cutout_size] = cutout
+    B[0, 0, :l_cutout_size-1, :l_cutout_size-1] = np.random.normal(0,1)
+
+    B_idx[0, 0, :l_cutout_size, :l_cutout_size] = torch.zeros(1)
 
     return B_idx, B
