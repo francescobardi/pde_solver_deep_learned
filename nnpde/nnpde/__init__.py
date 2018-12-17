d5cd54a5a0e60567e75d8a8494020edb1e4e4e4e
Samuel von Baussnern <samuel.vonbaussnern@gmail.com>
2018-12-17 15:47:09 +0100

    fixes bug with normal distr

---
+++ nnpde/nnpde/__init__.py
@@ -0,0 +1,6 @@
+__author__ = "Francesco Bardi, Samuel von Baussnern"
+__credits__ = ["Francesco Bardi",
+               "Samuel von Baussnern"]
+__license__ = "GPL"
+__maintainer__ = "Francesco Bardi"
+__status__ = "Development"
