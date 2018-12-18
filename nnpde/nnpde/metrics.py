c0d4c2ca94f4d720bf157aef4efb461dd3e2f2a9
Samuel von Baussnern <samuel.vonbaussnern@gmail.com>
2018-12-18 12:26:39 +0100

    adds seeds and some random refactoring

--- nnpde/nnpde/metrics.py
+++ nnpde/nnpde/metrics.py
@@ -4,19 +4,18 @@ import nnpde.functions.iterative_methods as im
 
 
 def compute_loss(net, problem_instances):
-    """ Fucntion to compute the total loss given a set of problem instances"""
+    """ Function to compute the total loss given a set of problem instances"""
 
     loss = torch.zeros(1)
 
     for problem_instance in problem_instances:
-
         ground_truth = problem_instance.ground_truth
 
         # Compute solution
         u = problem_instance.compute_solution(net)
 
-        # setting `reduction <- None` will result in square L2-Norm,
+        # setting `reduction <- 'sum'` will result in square L2-Norm,
         # aka Least Square Loss
-        loss = loss + F.mse_loss(ground_truth, u, reduction=None)
+        loss += F.mse_loss(ground_truth, u, reduction='sum')
 
     return loss
