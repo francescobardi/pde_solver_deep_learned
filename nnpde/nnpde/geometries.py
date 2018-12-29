5d900a5fb2d99bfec294d7a761baf143968dc976
Samuel von Baussnern <samuel.vonbaussnern@gmail.com>
2018-12-29 12:49:23 +0100

    improves styling of geometries

--- nnpde/nnpde/geometries.py
+++ nnpde/nnpde/geometries.py
@@ -4,7 +4,7 @@ import torch
 
 def square_geometry(size):
     """Defines a square geometry of given size (as a single positive integer)
-    The boundary-values are set randomly.
+    The boundary-values are set randomly from a uniform distribtion in the interval [-1, 1].
 
     Example:
 
@@ -18,18 +18,18 @@ def square_geometry(size):
 
     # Define geometry 1.0 inner points 0.0 elsewhre
     B_idx = torch.ones(1, 1, size ,size )
-    B_idx[0,0,0 ,:] = torch.zeros(size)
-    B_idx[0,0,-1,:] = torch.zeros(size)
-    B_idx[0,0,:, 0] = torch.zeros(size)
-    B_idx[0,0,:,-1] = torch.zeros(size)
+    B_idx[0, 0,  0,  :] = torch.zeros(size)
+    B_idx[0, 0, -1,  :] = torch.zeros(size)
+    B_idx[0, 0,  :,  0] = torch.zeros(size)
+    B_idx[0, 0,  :, -1] = torch.zeros(size)
 
     # Define boundary values
     B = torch.zeros_like(B_idx)
 
-    B[0,0,0 ,:] =  np.random.uniform(-1, 1)
-    B[0,0,:,-1] =  np.random.uniform(-1, 1)
-    B[0,0,-1,:] =  np.random.uniform(-1, 1)
-    B[0,0,1:, 0] = np.random.uniform(-1, 1) # we don't want to overwrite the first value
+    B[0, 0,  0, :] = np.random.uniform(-1, 1)
+    B[0, 0,  :,-1] = np.random.uniform(-1, 1)
+    B[0, 0, -1, :] = np.random.uniform(-1, 1)
+    B[0, 0, 1:, 0] = np.random.uniform(-1, 1) # we don't want to overwrite the first value
     return B_idx, B
 
 
@@ -43,13 +43,13 @@ def l_shaped_geometry(size, l_cutout_size=None):
         (geometry, boundary_index)
     """
 
-    l_cutout_size = l_cutout_size or int(np.floor(size/2))
+    l_cutout_size = l_cutout_size or int(np.floor(size / 2))
 
     B_idx, B = square_geometry(size)
 
     _, cutout = square_geometry(l_cutout_size)
     B[0, 0, :l_cutout_size, :l_cutout_size] = cutout
-    B[0, 0, :l_cutout_size-1, :l_cutout_size-1] = np.random.uniform(0,1)
+    B[0, 0, :l_cutout_size - 1, :l_cutout_size - 1] = np.random.uniform(0, 1)
 
     B_idx[0, 0, :l_cutout_size, :l_cutout_size] = torch.zeros(1)
 
