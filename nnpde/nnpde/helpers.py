cleans up helpers, fixed flops computation, adds general remarks

nnpde/nnpde/helpers.py
@@ -1,12 +1,15 @@
-# -*- coding: utf-8 -*-
-
+from itertools import product
 from functools import reduce
 
 import numpy as np
 
 
 def check_dimensions(A, f):
-    """Check the dimensions of inputs"""
+    """check_dimensions for matrix computation.
+
+    :param A: Matrix
+    :param f: Another matrix
+    """
 
     Na, Ma = np.shape(A)
     Nf = np.shape(f)[0]
@@ -17,30 +20,47 @@ def check_dimensions(A, f):
         raise ValueError("Dimensions mismatch, please check your inputs.")
 
 
+def conv_layer_to_matrix(conv_layer, dim, kernel_size=3):
+    """conv_layer_to_matrix Converts a convolutional layer to a matrix.
+
+    :param conv_layer: (PyTorch tensor) Convolutional layer to convert
+    :param dim: Dimension of matrix on which the convolution is to be applied
+    :returns: Matrix expression of convolutional layer
+    """
+    # TODO remove hardwiring to kernel size
+    if kernel_size != 3:
+        raise NotImplementedError("Kernel size of size different of 3 is currently not supported.")
+
+    ks = [k.item() for k in conv_layer.view(9)]
+
+    H = np.diag(np.ones(dim**2)) * ks[4] + np.diag(np.ones(dim**2 - 1), 1) * ks[5] \
+        + np.diag(np.ones(dim**2 - dim), dim) * ks[7] + np.diag(np.ones(dim**2 - dim - 1), dim + 1) * ks[8] \
+        + np.diag(np.ones(dim**2 - 1), -1) * ks[3] + np.diag(np.ones(dim**2 - dim + 1), dim - 1) * ks[6] \
+        + np.diag(np.ones(dim**2 - dim), -dim) * ks[1] + np.diag(np.ones(dim**2 - dim + 1), -dim + 1) * ks[2] \
+        + np.diag(np.ones(dim**2 - dim - 1), -dim - 1) * ks[0]
+
+    return H
+
+
 def conv_net_to_matrix(net, N):
-    Hs = []
+    """conv_net_to_matrix Expresses a linear convolutional net of kernel size 3 as a matrix,
+    to be used in a matrix multiplication.
 
-    for param in net.parameters():
-        ks = param.view(9)
-        ks = list(map(lambda el: el.item(), ks))
-
-        H = np.diag(np.ones(N**2)) * ks[4] + np.diag(np.ones(N**2-1), 1) * ks[5] \
-            + np.diag(np.ones(N**2 - N), N) * ks[7] + np.diag(np.ones(N**2 - N - 1), N+1) * ks[8] \
-            + np.diag(np.ones(N**2 - 1), -1) * ks[3] + np.diag(np.ones(N**2 - N+1), N-1) * ks[6] \
-            + np.diag(np.ones(N**2 - N), -N) * ks[1] + np.diag(np.ones(N**2 - N+1), -N+1) * ks[2] \
-            + np.diag(np.ones(N**2 - N - 1), -N-1) * ks[0]
-
-        Hs.append(H)
-
-    return reduce(lambda acc, el: np.dot(acc, el), Hs)
+    :param net: PyTorch linear convolutional network with an arbitrary number of layer.
+    :param N: Dimension of matrix on which the convolution is to be applied.
+    """
+    return reduce(lambda acc, el: np.dot(acc, el), (conv_layer_to_matrix(param, N) in net.parameters()))
 
 
 def build_T(N):
-    """ Build Jacobi method updated matrix for Poisson problem """
+    """build_T Build Jacobi method updated matrix for Poisson problem.
+
+    :param N: Size of matrix.
+    """
 
     # Build diagonals
-    a = np.ones(N**2-1)*0.25
-    b = np.ones(N**2-N)*0.25
+    a = np.ones(N**2 - 1) * 0.25
+    b = np.ones(N**2 - N) * 0.25
 
     # Build T
     T = np.diag(a, 1) + np.diag(a, -1) + np.diag(b, N) + np.diag(b, -N)
@@ -49,7 +69,10 @@ def build_T(N):
 
 
 def build_G(B_idx):
-    """ Build reset boundary matrix """
+    """build_G Build reset boundary matrix.
+
+    :param B_idx: "Boolean" Index of boundary.
+    """
 
     N = B_idx.size()[2]
     M = B_idx.size()[3]
@@ -57,71 +80,67 @@ def build_G(B_idx):
     B_idx = B_idx.view(N, M).numpy()
 
     a = np.zeros(N**2)
-    for i in range(N):
-        for j in range(N):
-            a[i*N + j] = B_idx[i, j]
+    for i, j in product(range(N), range(N)):
+        a[i * N + j] = B_idx[i, j]
 
     G = np.diag(a)
     return G
 
 
 def spectral_radius(X):
-    """ Compute the spectral radius of a matrix """
+    """spectral_radius Computes the spectral radius of matrix X.
 
-    # Compute eigenvalues
-    eigvals = np.linalg.eigvals(X)
-
-    return np.max(np.absolute(eigvals))
-
-
-def count_conv(grid_size, kernel_size, nb_layers=3):
+    :param X:
     """
-    Parameters
-    -----------
-    grid_size:
-        input_shape shape of the input matrix an integer since we have a square matrix
+    return np.max(np.abs(np.linalg.eigvals(X)))
+
+
+def n_flops_conv_net_step(grid_size, kernel_size, n_layers=3):
+    """n_flops_conv_net_step Estimation on the number of flops taken by the convolution.
+
+    Counting multiply-and-add computations as one flop.
+
+    :param grid_size:
+        grid size on which the problem is solved
+    :param kernel_size:
         kernel size shape of convolution kernel
-        number of layer number of convolution nb_layers
-
-    Returns
-    ----------
-    number of flops (int)
+    :param n_layers:
+        (int) number of layer number of convolution n_layers
+    :returns:
+        (int) number of flops
     """
 
-    nb_add_mult = nb_layers * (kernel_size**2 * grid_size**2)
-
-    return nb_add_mult
+    return n_layers * (kernel_size**2 * grid_size**2)
 
 
-def count_jac(grid_size: int) -> float:
-    """
-    Parameters
-    -----------
-        input_shape (int) : shape of input
-    Returns
-    ----------
-        number of flops (float)
+def n_flops_jacoby_step(grid_size):
+    """n_flops_jacoby_step Estimation of the number of flops taken by the Jacoby Solver.
+
+    :param grid_size:
+        (int) grid size on which the problem is solved
+    :returns:
+        (int) number of flops
     """
 
     return 4 * grid_size**2
 
 
-def compare_flops(grid_size, n_iter_jac, n_iter_conv, nb_layers):
-    """
-    Parameters:
-    -----------
+def flops_ratio(grid_size, n_iter_jac, n_iter_conv, n_layers):
+    """flops_ratio
 
-        grid_size (int): Dimension of the square matrix dim x dim.
-        n_iter_conv (int): Number of iterations it took for the trained model to converge
-        n_iter_jac (int): Number of iterations it took for the jacobi method to converge to the ground truth solution
-
-    Returns:
-    ----------
+    :param grid_size:
+        (int) Dimension of the square matrix dim x dim.
+    :param n_iter_jac:
+        (int) Number of iterations it took for the trained model to converge
+    :param n_iter_conv:
+        (int) Number of iterations it took for the jacobi method to converge to the ground truth solution
+    :param n_layers:
+        (int) Number of layers used in the convolutional net.
+    :returns:
        ratio of the number of flops for the convolution and jacobi method
-
     """
-    flop_jac = count_jac(grid_size) * n_iter_jac
+    flop_jac = n_flops_jacoby_step(grid_size) * n_iter_jac
 
-    flop_conv = flop_jac + count_conv(grid_size, nb_layers) * n_iter_conv
+    flop_conv = (flop_jac + n_flops_conv_net_step(grid_size, n_layers)) * n_iter_conv
 
-    return flop_conv/flop_jac
+    return flop_conv / flop_jac

nnpde/nnpde/model_testing.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 
 from nnpde.metrics import least_squares_loss as LSE
-from nnpde.helpers import compare_flops
+from nnpde.helpers import flops_ratio
 from nnpde.problems import DirichletProblem
 import nnpde.iterative_methods as im
 from nnpde import metrics
@@ -35,7 +35,7 @@ def _test_model_(model, n_tests, grid_size, tol=1e-6, max_nb_iters=50000, conver
     initial_u = torch.ones(1, 1, grid_size, grid_size)
 
     for i in range(n_tests):
-        # TODO This is stupid, it should not be here...
+        # TODO This is stupid, it should not be here... the problems should be instantiated outside
         problem_instance = DirichletProblem(
             N=grid_size, k_ground_truth=max_nb_iters, domain_type=domain_type)
         ground_truth = problem_instance.ground_truth
@@ -100,8 +100,8 @@ def _test_model_(model, n_tests, grid_size, tol=1e-6, max_nb_iters=50000, conver
         con_time = time.process_time() - start_time
         logging.debug(f'Test nb {i} done')
 
-        # TODO this is stupid
-        yield (count_jac, count_H, compare_flops(grid_size, count_jac, count_H, nb_layers), jac_time, con_time,
+        # TODO this is stupid, too many return values.
+        yield (count_jac, count_H, flops_ratio(grid_size, count_jac, count_H, nb_layers), jac_time, con_time,
                con_time / jac_time, loss_jac.item(), loss_H.item())
 
 